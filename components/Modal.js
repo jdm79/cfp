53a4524884d1f4fe801dcbbe14df835bfdbf8704
jdm79 <james.malvern@gmail.com>
2023-08-11 14:32:48 +0100

    add todos to list

--- components/Modal.js
+++ components/Modal.js
@@ -1,9 +1,11 @@
+import { useAuth } from "@/context/AuthContext";
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 export default function Modal(props) {
   const { setOpenModal } = props;
   const [_document, set_document] = useState(null);
+  const { logout } = useAuth();
 
   useEffect(() => {
     set_document(document);
@@ -14,16 +16,26 @@ export default function Modal(props) {
   }
 
   return ReactDOM.createPortal(
-    <div className='fixed inset-0 bg-white text-slate-900 flex flex-col '>
+    <div className='fixed inset-0 bg-white text-slate-900 flex flex-col text-lg sm:text-xl'>
       <div className='flex items-center justify-between border-b border-solid border-slate-900 p-4'>
-        <h1>Menu</h1>
+        <h1 className='font-extrabold text-2xl sm:text-5xl select-none'>
+          Menu
+        </h1>
         <i
           onClick={() => setOpenModal(false)}
-          className='fa-solid fa-xmark'
+          className='fa-solid fa-xmark duration-300 hover:rotate-90 cursor-pointer text-lg sm:text-3xl'
         ></i>
       </div>
-      <div className='p-4 flex flex-col gap-3 '>
-        <h2>Logout</h2>
+      <div className='p-4 flex flex-col gap-3'>
+        <h2
+          className='select-none duration-300 hover:pl-2 cursor-pointer'
+          onClick={() => {
+            logout();
+            setOpenModal(false);
+          }}
+        >
+          Logout
+        </h2>
       </div>
     </div>,
     _document.getElementById("portal")
--- components/UserDashboard.js
+++ components/UserDashboard.js
@@ -1,5 +1,67 @@
-import React from "react";
+import { useAuth } from "@/context/AuthContext";
+import React, { useState, useEffect } from "react";
 
 export default function UserDashboard() {
-  return <div>UserDashboard</div>;
+  const { userInfo } = useAuth();
+  const [addTodo, setAddTodo] = useState(false);
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState({});
+  console.log(todoList);
+
+  useEffect(() => {
+    if (!userInfo || Object.keys(userInfo).length === 0) {
+      setAddTodo(true);
+    }
+  }, [userInfo]);
+
+  async function handleAddTodo() {
+    if (!todo) {
+      return;
+    }
+    const newKey =
+      Object.keys(todoList).length === 0
+        ? 1
+        : Math.max(...Object.keys(todoList)) + 1;
+    setTodoList({
+      ...todoList,
+      [newKey]: todo,
+    });
+  }
+
+  return (
+    <div className='w-full max-w-[65ch] text-xs sm:text-sm mx-auto flex flex-col gap-3 sm:gap-5'>
+      {addTodo && (
+        <div className='flex items-stretch'>
+          <input
+            type='text'
+            placeholder='Enter todo'
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            className='flex-1 outline-none p-3 text-base sm:text-lg text-slate-900'
+          />
+          <button
+            onClick={handleAddTodo}
+            className='w-fit px-4 sm:px-6 py-2 sm:py-3 bg-amber-400 text-white uppercase font-medium text-base duration-300 hover:opacity-40'
+          >
+            Add
+          </button>
+        </div>
+      )}
+      {userInfo && (
+        <>
+          {Object.keys(todoList).map((todo, i) => {
+            return <div key={i}>{todoList[todo]}</div>;
+          })}
+        </>
+      )}
+      {!addTodo && (
+        <button
+          onClick={() => setAddTodo(true)}
+          className='text-cyan-300 border border-solid border-cyan-300 py-2 text-center uppercase text-lg duration-300 hover:opacity-30 select-none'
+        >
+          ADD TODO
+        </button>
+      )}
+    </div>
+  );
 }
